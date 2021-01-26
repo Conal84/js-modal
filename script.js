c24724b1078c68a18867fb2f2ae2e8bdbc436768
Conal Walsh <conal.walsh1@gmail.com>
2021-01-26 19:24:58 +0000

    Added DOM element selections and function definitions to script

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// Select DOM elements
+const showModalBtn = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+console.log(showModalBtn);
+
+// Function definitions
+function openModal() {
+  if (modal.classList.contains('hidden')) {
+    modal.classList.remove('hidden');
+  }
+  if (overlay.classList.contains('hidden')) {
+    overlay.classList.remove('hidden');
+  }
+}
+
+function hideModal() {
+  if (!modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+  }
+  if (!overlay.classList.contains('hidden')) {
+    overlay.classList.add('hidden');
+  }
+}
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', openModal);
+}
+
+closeModal.addEventListener('click', hideModal);
+
+overlay.addEventListener('click', hideModal);
