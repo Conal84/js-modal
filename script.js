62aefe32c07a60d2881afe8cb3590fb9a953abae
Conal Walsh <conal.walsh1@gmail.com>
2021-01-26 19:40:07 +0000

    Added js event listeners to close the modal

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-console.log(showModalBtn);
-
-// Function definitions
+// A function to open the modal
 function openModal() {
   if (modal.classList.contains('hidden')) {
     modal.classList.remove('hidden');
@@ -18,6 +16,7 @@ function openModal() {
   }
 }
 
+// A function to close the modal
 function hideModal() {
   if (!modal.classList.contains('hidden')) {
     modal.classList.add('hidden');
@@ -27,10 +26,16 @@ function hideModal() {
   }
 }
 
+// Add event listeners
 for (let i = 0; i < showModalBtn.length; i++) {
   showModalBtn[i].addEventListener('click', openModal);
 }
 
 closeModal.addEventListener('click', hideModal);
-
 overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    hideModal();
+  }
+});
